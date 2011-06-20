12586d117bbdb1ddfa88d55dc022f00b17162c6f
sean <sean@decrypted.org>
2011-06-19 23:22:58 -0700

    Added a skip regex parser test.

--- unittest/test_parser.py
+++ unittest/test_parser.py
@@ -3,6 +3,7 @@ import unittest
 from parser import parse_nzb
 
 class TestParser(unittest.TestCase):
+
     def setUp(self):
         self.nzb = StringIO.StringIO("""<?xml version="1.0" encoding="iso-8859-1" ?>
           <!DOCTYPE nzb PUBLIC "-//newzBin//DTD NZB 1.0//EN" "http://www.newzbin.com/DTD/nzb/nzb-1.0.dtd">
@@ -18,11 +19,19 @@ class TestParser(unittest.TestCase):
            </nzb>""")
 
     def test_parser(self):
-        """."""
+        """Test a regular parse."""
         size, queue, skipped = parse_nzb(self.nzb)
         self.assertEqual(size, 1234)
         self.assertEqual(len(queue), 1)
         self.assertEqual(len(skipped), 0)
+
+    def test_parser_skip(self):
+        """Tests the skip regex."""
+        size, queue, skipped = parse_nzb('test.nzb', ['par2'])
+        self.assertEqual(size, 8026)
+        self.assertEqual(len(queue), 3)
+        self.assertEqual(len(skipped), 1)
+        self.assertEqual(skipped[0]['file_subject'], 'file subject 4.par2')
 
 if __name__ == '__main__':
     unittest.main()
