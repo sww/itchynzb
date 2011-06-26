80fdda72975e211c4256f66af8c04ff67938fe08
sean <sean@decrypted.org>
2011-06-26 15:43:44 -0700

    Added more test cases and changed the formatting for the return value for get_size.

--- common/helper.py
+++ common/helper.py
@@ -1,19 +1,16 @@
-#
 """
-Miscellaneous helper functions.
+Miscellaneous helper/convenience functions.
 """
 import glob
 import os
 import re
 import sys
-
 from compressed import unzip
 
 def htime(time):
     """Returns a human readable time."""
     seconds = time % 60
     minutes = time / 60
-
     if minutes >= 60:
         hours = minutes/60
         minutes = minutes % 60
@@ -44,11 +41,10 @@ def get_filename_from(subject):
     """Return a filename from a subject."""
     # # Use multiple regexs because I can't figure one that works.
     res = re.match('.*?(?:"|&quot|&quot;|\s*)([\.0-9A-Za-z\[\]_\ \(\)]+\.\w+).*', subject)
-
     if res:
         return res.groups()[0]
 
-    return None
+    return ''
 
 def get_size(size, suffix_only=False):
     """Return a human readble size."""
@@ -60,14 +56,16 @@ def get_size(size, suffix_only=False):
         elif suffix_only:
             return suffix
         else:
-            return '%s %s' % (format(hsize, '3.2f'), suffix)
+            if hsize > 1:
+                return '%s %s' % (format(hsize, '3.1f'), suffix)
+            else:
+                return '%s %s' % (format(hsize, '3.2f'), suffix)
+    return ''
 
 def print_static(string, width=80):
     sys.stdout.write('\r%s%s' % (string, ' ' * (width-len(string))))
     sys.stdout.flush()
 
 if __name__ == '__main__':
-    #print get_filename_from_subject("SOME FILE 'NAMED.nfo'")
     print get_filename_from("""New subject name "Filename goes here (v5.0) (echo).rar" (1/3)""")
     print get_filename_from("""[U4A] 150411-22:59:42[01/26] - &quot;150411-22:59:42.par2&quot; yEnc (1/1)""")
-    #Print get_size(sys.argv[1])
--- unittest/test_common.py
+++ unittest/test_common.py
@@ -1,8 +1,10 @@
+import os
+import tempfile
 import unittest
 import common.helper as helper
-#import common.compressed as compressed
 
 class TestCommonFunctions(unittest.TestCase):
+
     def setUp(self):
         pass
 
@@ -19,12 +21,43 @@ class TestCommonFunctions(unittest.TestCase):
             ("""[1235]-[#chan@IrC]-[FULL]-[12345]-[21/79] - some.file.r01 (1/50)""", 'some.file.r01'),
             ("""[353]-[#chan@IrC]-[FULL]-[10202]-[21/79] - "some.file.r01" (1/50)""", 'some.file.r01'),
             ("""New subject name "Filename goes here (v5.0) (echo).rar" (1/3)""", 'Filename goes here (v5.0) (echo).rar'),
-            ("""Blank""", None)
+            ("""Blank""", '')
         ]
 
         for subject, expected_filename in test_subjects:
             filename = helper.get_filename_from(subject)
-            self.assertEqual(filename, expected_filename)
+            try:
+                self.assertEqual(filename, expected_filename)
+            except:
+                print subject
+                raise
+
+    def test_htime(self):
+        self.assertEqual(helper.htime(1234), '20m 34s')
+        self.assertEqual(helper.htime(53), '0m 53s')
+        self.assertEqual(helper.htime(3720), '1h 2m 0s')
+
+    def test_get_size(self):
+        self.assertEqual(helper.get_size(1000), '0.98 KB')
+        self.assertEqual(helper.get_size(25600), '25.0 KB')
+        self.assertEqual(helper.get_size(1024*1024*3), '3.0 MB')
+        self.assertEqual(helper.get_size(1024**3*2.3), '2.3 GB')
+        self.assertEqual(helper.get_size(1024**4*5.5), '5.5 TB')
+        self.assertEqual(helper.get_size(1234, True), 'KB')
+        self.assertEqual(helper.get_size(1024*1024*32, True), 'MB')
+        self.assertEqual(helper.get_size(1024**3*77, True), 'GB')
+        self.assertEqual(helper.get_size(1024**4*1.2, True), 'TB')
+        self.assertEqual(helper.get_size(0), '0.00 KB')
+
+    def test_get_nzb_file(self):
+        self.assertEqual(helper.get_nzb_file('.'), ['./gpl.nzb', './test.nzb'])
+        tempdir = tempfile.mkdtemp()
+        self.assertEqual(helper.get_nzb_file(tempdir), [])
+        os.rmdir(tempdir)
+
+    def test_get_download_path(self):
+        self.assertEqual(helper.get_download_path('', 'test.nzb'), 'test')
+        self.assertEqual(helper.get_download_path('dir/', 'test.nzb'), 'dir/test')
    
 if __name__ == '__main__':
     unittest.main()
