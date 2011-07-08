c156d2f6609c42dc41c4a179daf0e51274e79622
sean <sean@decrypted.org>
2011-07-07 22:01:35 -0700

    Changed config to enforce a certain set of keys exist and added a unit test.

--- config.py
+++ config.py
@@ -1,17 +1,26 @@
-#
 import json
-from os.path import exists
 
 def read_config(config='settings.conf'):
-    if not exists(config):
-        raise IOError('Could not find settings file %s.' % config)
-    
-    try:
-        settings = json.loads(open(config).read())
-    except ValueError:
-        raise
-    finally:
-        return settings
+    with open(config) as cfg:
+        settings = json.loads(cfg.read())
+
+    keys = [
+        'host',
+        'username',
+        'password',
+        'port',
+        'connections',
+        'download_dir',
+        'temp_dir',
+        'skip_regex'
+    ]
+
+    # Enforce that the config has the right set of keys.
+    for key in keys:
+        if key not in settings:
+            raise KeyError('Config does not have key "%s"' % key)
+
+    return settings
 
 if __name__ == '__main__':
     import sys
